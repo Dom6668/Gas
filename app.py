1b4a75f2ebfd1f7a55be58b733b4ea0cbbcd79fb
Dom6668 <mute66@gmail.com>
2026-04-25 09:04:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ if not results.empty:
     display_df['Price (¢)'] = display_df.apply(make_clickable_price, axis=1)
     
     # Final column selection for the table
-    final_table = display_df[['Price (¢)', 'Address', 'brand']]
+    final_table = display_df[['Price (¢)', 'Address', 'Brand']]
     
     # Displaying as Markdown to allow the hyperlink
     st.markdown(final_table.to_markdown(index=False))
